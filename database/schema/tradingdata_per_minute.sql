88c54206ebf1af52fca3db31cd4e3de37d794ecf
Marco Scholz <mail@marco-scholz.com>
2025-05-23 15:25:56 +0200

    tradingdata_per_minute

---
+++ database/schema/tradingdata_per_minute.sql
@@ -0,0 +1,20 @@
+CREATE TABLE quicksight.tradingdata_by_minute
+(
+    trading_day    DATE,
+    trading_minute TIMESTAMP WITHOUT TIME ZONE,
+    time_of_day    TIME(0),
+    exchange_id    int,
+    account_id     int,
+    turnover_usd   NUMERIC(20, 2),
+    tpl1_usd       NUMERIC(20, 2),
+    tpl10_usd      NUMERIC(20, 2),
+    tpl30_usd      NUMERIC(20, 2),
+    tpl60_usd      NUMERIC(20, 2),
+    tpl300_usd     NUMERIC(20, 2),
+    tpl900_usd     NUMERIC(20, 2),
+    trade_count    INTEGER,
+    updated_ts     TIMESTAMP WITHOUT TIME ZONE,
+    PRIMARY KEY (trading_minute, account_id)
+);
+
+CREATE INDEX tradingdata_day_time_idx ON tradingdata_by_minute (time_of_day, trading_day, account_id);
