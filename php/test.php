c334130ce38c2ff0451aa0349210a5465840ea3e
Marco Scholz <mail@marco-scholz.com>
2025-05-23 11:38:58 +0200

    initial

---
+++ php/test.php
@@ -0,0 +1,7 @@
+#!/usr/bin/env php
+<?php
+
+use AlphaRock\Core\Config\Config;
+
+require_once __DIR__ . "/../vendor/autoload.php";
+new Config(__DIR__ . "/../");
---
+++ sql/_test.sql
@@ -0,0 +1,1 @@
+SELECT * FROM live.strategy_instances LIMIT 1
